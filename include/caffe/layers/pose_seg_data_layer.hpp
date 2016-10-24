0aff854d5c518d0b7e75877c2659f6552bdeeb18
Engineering-Course <gk12330088@gmail.com>
2016-10-24 21:37:05 +0800

    pose_seg unfinished

---
+++ include/caffe/layers/pose_seg_data_layer.hpp
@@ -0,0 +1,43 @@
+#ifndef CAFFE_POSE_SEG_DATA_LAYER_HPP_
+#define CAFFE_POSE_SEG_DATA_LAYER_HPP_
+
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/data_transformer.hpp"
+#include "caffe/internal_thread.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/layers/base_data_layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+
+namespace caffe {
+
+template <typename Dtype>
+class PoseSegDataLayer : public ImageDimPrefetchingDataLayer<Dtype> {
+ public:
+  explicit PoseSegDataLayer(const LayerParameter& param)
+    : ImageDimPrefetchingDataLayer<Dtype>(param) {}
+  virtual ~PoseSegDataLayer();
+  virtual void DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline int ExactNumBottomBlobs() const { return 0; }
+  virtual inline int ExactNumTopBlobs() const { return 3; }
+  virtual inline bool AutoTopBlobs() const { return true; }
+
+ protected:
+  virtual void ShuffleImages();
+  virtual void load_batch(Batch<Dtype>* batch);
+
+  Blob<Dtype> transformed_label_;
+  shared_ptr<Caffe::RNG> prefetch_rng_;
+  vector<std::pair< std::pair<std::string, std::string>, vector<float> > lines_;
+  int lines_id_;
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_POSE_SEG_DATA_LAYER_HPP_
